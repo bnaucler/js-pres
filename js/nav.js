64b789e96d953dda2d7ec869ee6297208694694f
bnaucler <mail@bnaucler.se>
2017-09-13 20:45:50 +0200

    Adding comments to nav.js

--- js/nav.js
+++ js/nav.js
@@ -1,26 +1,36 @@
-document.getElementById('head').innerHTML = document.title;
+// github.com/bnaucler/js-pres
 
-var cpage = window.location.pathname.split("/").pop();
-var prevpage, nextpage;
-var plen = page.length;
+var cpage = window.location.pathname.split("/").pop(); // Get name of current file
+var prevpage, nextpage; // Create variables to hold names of prev and next page
+var plen = page.length; // Calcluate number of pages to loop through
 
-for(var i = 0; i < plen; i++) {
-    if(page[i] === cpage) {
-        prevpage = page[(i - 1)];
-        nextpage = page[(i + 1)];
-        break;
+for(var i = 0; i < plen; i++) {     // Use 'i' to count to 'plen'
+    if(page[i] === cpage) {         // Check if page[i] has the same name as cpage
+        prevpage = page[(i - 1)];   // Set previous page
+        nextpage = page[(i + 1)];   // Set next page
+        break;                      // No need to keep looping
     }
 }
 
-if(prevpage == undefined)
+// 'gopage' is executed when a navigational arrow is clicked
+// 'page' can now be either 'prevpage' or 'nextpage'
+function gopage(page) {
+    if(page) window.location = page; // if page is defined: go there!
+}
+
+// hide the arrows if they don't point anywhere
+if(prevpage === undefined)
     document.getElementById('backarrow').style.visibility = 'hidden';
 
-if(nextpage == undefined)
+if(nextpage === undefined)
     document.getElementById('fwdarrow').style.visibility = 'hidden';
 
-function gopage(page) {
-    if(page != undefined) window.location.href = page;
-}
+
+
+
+
+
+document.getElementById('head').innerHTML = document.title;
 
 document.onkeyup = function(evt) {
     if(evt.keyCode == 37) gopage(prevpage);
